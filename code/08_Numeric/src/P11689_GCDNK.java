015f43a3e0f54ce6422d9bd6bca04dc527f253b8
lim-it-err <jhlim271@naver.com>
2024-02-06 18:43:10 +0900

    wip

---
+++ code/08_Numeric/src/P11689_GCDNK.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class P11689_GCDNK {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long result = n;
+        for (long p = 2 ; p <= Math.sqrt(n); p++){
+            if (n%p ==0){
+                result -= result/p;
+                while (n%p ==0) n/=p;
+            }
+        }
+        if (n>1)    result-=result/n;
+        System.out.println(result);
+    }
+}
--- code/08_Numeric/src/P45_axbyc.java
+++ code/08_Numeric/src/P45_axbyc.java
@@ -26,6 +26,7 @@ public class P45_axbyc {
 
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
+
             int A = sc.nextInt();
             int B = sc.nextInt();
             int C = sc.nextInt();
